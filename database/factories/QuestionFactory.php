f2ebe17edd5529916cf54d3922bb3d32525d91ce
BrunoSantos-07 <brunosantos.dev07@gmail.com>
2025-05-26 10:27:02 -0300

    HEY-9: adding the option to auto login when isLocal()

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -2,7 +2,6 @@
 
 namespace Database\Factories;
 
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,9 +17,7 @@ class QuestionFactory extends Factory
     public function definition(): array
     {
         return [
-            'question'   => fake()->realText(50),
-            'draft'      => fake()->boolean(),
-            'created_by' => User::factory(),
+            'question' => fake()->realText(50),
         ];
     }
 }
