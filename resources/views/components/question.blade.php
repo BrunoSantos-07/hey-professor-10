5f61f3c57a4098ea6b9af10ed17f20a3e2a30afc
BrunoSantos-07 <brunosantos.dev07@gmail.com>
2025-06-04 20:31:34 -0300

    hey-11: handling N+1 issue

--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -8,13 +8,13 @@
         <x-form :action="route('question.like', $question)">
             <button class="flex items-start space-x-1">
                 <x-icons.thumb-up id="thumb-up" class="houver:text-green-300 h-5 w-5 cursor-pointer text-green-500"/>
-                <span>{{ $question->likes }}</span>
+                <span>{{ $question->votes_sum_like ?: 0 }}</span>
             </button>
         </x-form>
         <x-form :action="route('question.unlike', $question)">
             <button class="flex items-start space-x-1">
                 <x-icons.thumb-down id="thumb-down" class="houver:text-red-300 h-5 w-5 cursor-pointer text-red-500"/>
-                <span>{{ $question->unlikes }}</span>
+                <span>{{ $question->votes_sum_unlike ?: 0 }}</span>
             </button>
         </x-form>
     </div>
