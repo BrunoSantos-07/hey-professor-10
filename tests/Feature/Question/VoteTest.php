73eecb368ef0684cf111d8cf9b031e235ce862e5
BrunoSantos-07 <brunosantos.dev07@gmail.com>
2025-06-04 13:38:46 -0300

    hey-11: it should not be able to like more than 1 time

--- tests/Feature/Question/VoteTest.php
+++ tests/Feature/Question/VoteTest.php
@@ -4,9 +4,11 @@ use App\Models\{Question, User};
 
 use function Pest\Laravel\{actingAs, assertDatabaseHas, post};
 
-it("should be able to like a question", function () {
+it('should be able to like a question', function () {
     // Arrange
-    $user     = User::factory()->create();
+    /** @var User $user */
+    $user = User::factory()->create();
+    /** @var Question $question */
     $question = Question::factory()->create();
 
     actingAs($user);
@@ -21,4 +23,23 @@ it("should be able to like a question", function () {
         'unlike'      => 0,
         'user_id'     => $user->id,
     ]);
+});
+
+it('should not be able to like more than 1 time', function () {
+    // Arrange
+    /** @var User $user */
+    $user = User::factory()->create();
+    /** @var Question $question */
+    $question = Question::factory()->create();
+
+    actingAs($user);
+
+    // Act
+    post(route('question.like', $question));
+    post(route('question.like', $question));
+    post(route('question.like', $question));
+    post(route('question.like', $question));
+
+    // Assert
+    expect($user->votes()->where('question_id', '=', $question->id)->get())->toHaveCount(1);
 });
