c7438a68a7346fadcaadc069635a2b8db19b70f1
BrunoSantos-07 <brunosantos.dev07@gmail.com>
2025-06-06 11:33:31 -0300

    hey-14: it should be able to destroy a question

---
+++ resources/views/components/table/thead.blade.php
@@ -0,0 +1,5 @@
+<thead class="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
+    <tr>
+        {{ $slot }}
+    </tr>
+</thead>
--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -15,16 +15,69 @@
             </x-btn.cancel>
         </x-form>
 
-        <hr class="my-4 border-dashed border-gray-700"/>
+        <hr class="my-4 border-dashed border-gray-700" />
+
+        <div class="mb-1 font-bold uppercase dark:text-gray-300">
+            Drafts
+        </div>
+
+        <x-table>
+            <x-table.thead>
+                <x-table.th>
+                    Question
+                </x-table.th>
+                <x-table.th>
+                    Actions
+                </x-table.th>
+            </x-table.thead>
+            <tbody>
+                @foreach ($questions->where('draft', true) as $question)
+                    <x-table.tr>
+                        <x-table.td>
+                            {{ $question->question }}
+                        </x-table.td>
+                        <x-table.td>
+                            <x-form :action="route('question.publish', $question)" put>
+                                <button type="submit" class="houver:underline text-blue-500">Publicar</button>
+                            </x-form>
+                            <x-form :action="route('question.destroy', $question)" delete>
+                                <button type="submit" class="houver:underline text-red-500">Delete</button>
+                            </x-form>
+                        </x-table.td>
+                    </x-table.tr>
+                @endforeach
+            </tbody>
+        </x-table>
+
+        <hr class="my-4 border-dashed border-gray-700" />
 
         <div class="mb-1 font-bold uppercase dark:text-gray-300">
             My Questions
         </div>
 
-        <div class="space-y-4 dark:text-gray-400">
-            @foreach ($questions as $item)
-                <x-question :question="$item"/>
-            @endforeach
-        </div>
+        <x-table>
+            <x-table.thead>
+                <x-table.th>
+                    Question
+                </x-table.th>
+                <x-table.th>
+                    Actions
+                </x-table.th>
+            </x-table.thead>
+            <tbody>
+                @foreach ($questions->where('draft', false)  as $question)
+                    <x-table.tr>
+                        <x-table.td>
+                            {{ $question->question }}
+                        </x-table.td>
+                        <x-table.td>
+                            <x-form :action="route('question.destroy', $question)" delete>
+                                <button type="submit" class="houver:underline text-red-500">Delete</button>
+                            </x-form>
+                        </x-table.td>
+                    </x-table.tr>
+                @endforeach
+            </tbody>
+        </x-table>
     </x-container>
 </x-app-layout>
